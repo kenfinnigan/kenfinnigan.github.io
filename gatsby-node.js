87a8914c32113912a4d27cbca2a49f5b0048b31b
Ken Finnigan <ken@kenfinnigan.me>
2024-02-19 10:33:36 -0500

    Publish blog post on reproducers in open source
    Signed-off-by: Ken Finnigan <ken@kenfinnigan.me>

--- gatsby-node.js
+++ gatsby-node.js
@@ -197,7 +197,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   })
 
   // Create Blog Post list
-  const postsPerPage = 6
+  const postsPerPage = 12
   const numPages = Math.ceil(posts.length / postsPerPage)
   Array.from({ length: numPages }).forEach((_, i) => {
     createPage({
